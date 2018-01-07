184387c00604a3a331ee74c659b2195549df0a35
James Martineau <James.Martineau@libertymutual.com>
2018-01-07 17:55:53 -0500

    basic ORM.js update

--- config/orm.js
+++ config/orm.js
@@ -4,7 +4,7 @@ var connection = require("./connection.js");
 // Helper function for SQL syntax.
 // Let's say we want to pass 3 values into the mySQL query.
 // In order to write the query, we need 3 question marks.
-// The above helper function loops through and creates an array of question marks - ["?", "?", "?"] - and turns it into a string.
+// The helper function loops through and creates an array of question marks - ["?", "?", "?"] - and turns it into a string.
 // ["?", "?", "?"].toString() => "?,?,?";
 function printQuestionMarks(num) {
   var arr = [];
@@ -54,10 +54,10 @@ var orm = {
     var queryString = "INSERT INTO " + table;
 
     queryString += " (";
-    queryString += cols.toString();
+    queryString += cols.toString();//column names
     queryString += ") ";
     queryString += "VALUES (";
-    queryString += printQuestionMarks(vals.length);
+    queryString += printQuestionMarks(vals.length);//insert # of ?'s as there are values being insertedd
     queryString += ") ";
 
     console.log(queryString);
@@ -67,7 +67,7 @@ var orm = {
         throw err;
       }
 
-      cb(result);
+      cb(result);//callback
     });
   },
   // An example of objColVals would be {name: panther, sleepy: true}
